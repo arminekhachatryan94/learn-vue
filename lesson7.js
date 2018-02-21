b3ec741cbf65b0a2a17a36634e84700e7d1aa82a
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-02-21 10:02:24 -0800

    next 4 lessons

---
+++ lesson7.js
@@ -0,0 +1,16 @@
+/* global */
+Vue.component('task', {
+    template: '<li><slot></slot></li>'
+    
+    /*
+    data() {
+        return {
+            message: 'Foobar'
+        };
+    }
+    */
+});
+
+new Vue({
+    el: '#root'
+});
